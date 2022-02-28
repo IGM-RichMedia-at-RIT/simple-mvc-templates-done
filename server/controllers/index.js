9675a030d7c1106963ce6eb2d8ce3cf38b1876ec
Austin Willoughby <arwigm@rit.edu>
2022-02-28 15:01:02 -0500

    Added information about built-in helpers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -29,8 +29,17 @@ const hostPage1 = (req, res) => {
   // type in the app.js as handlebars. Calling res.render('index')
   // actually calls index.handlebars. A second parameter of JSON can be passed
   // into the handlebars to be used as variables with #{varName}
-  res.render('page1', {
+  const myArray = ['Austin', 'Cody', 'Binky', 'Aron'];
+  const filtered = myArray.filter(x => x.length <= 4);
+
+  // In this example, our page1.handlebars has a variable for looping over an array of
+  // names. We could give it the entire myArray, but if we want to limit the data we use
+  // our javascript controller code to filter it beforehand and then have the template
+  // display it. Even though handlebars supports some light logic with build in helpers, it
+  // is not the job of the view to perform logical operations.
+  return res.render('page1', {
     title: 'Page 1',
+    names: filtered,
   });
 };
 
